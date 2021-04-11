3654a4e8fabb80644cf6ee39fa4b35daf871616d
hellomuthu23 <hellomuthu23@gmail.com>
2021-04-11 23:55:58 +0100

    feat: add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './service/analytics';
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/service/analytics.ts
@@ -0,0 +1,4 @@
+import ReactGA from 'react-ga';
+
+const trackingId = 'G-LEK1503CC0';
+ReactGA.initialize(trackingId);
