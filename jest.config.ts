9010e5f89ae3c4b378ee90453e0b1d8024eba179
L4Ph <4ranci0ne@gmail.com>
2025-05-03 09:13:53 +0900

    fix: update Jest configuration to use Happy DOM and remove deprecated setup file

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testEnvironment: '@happy-dom/jest-environment',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
     '^.+\\.(t|j)sx?$': [
       '@swc/jest',
@@ -21,8 +21,6 @@ export default {
     ],
   },
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/__mocks__/fileMock.js',
-    '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+    '\\.(css|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
   },
 };
