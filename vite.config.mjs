ade2b86b4b63a51cb4680a02dc063cc38ff6500c
Muthu Pandian <hellomuthu23@gmail.com>
2025-05-15 18:45:19 +0100

    chore: switch to vite

---
+++ vite.config.mjs
@@ -0,0 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tailwindcss()],
+});
