5e307c2c987c4820483d14f4bded5b90417abd60
Muthu Pandian <hellomuthu23@gmail.com>
2025-06-14 18:40:25 +0100

    fix: fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+// eslint.config.js
+export default {
+  ignores: ['public/sw.js', 'build/'],
+  // Add your other config here, for example:
+  // extends: ['eslint:recommended', 'plugin:react/recommended'],
+};
