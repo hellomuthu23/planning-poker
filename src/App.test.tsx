2f309884a8b543eb6c229cf723d4eed19cf8421a
Muthu Pandian <hellomuthu23@gmail.com>
2025-06-15 00:05:35 +0100

    test: update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,11 @@ jest.mock('./service/games');
 jest.mock('./repository/firebase');
 
 // eslint-disable-next-line jest/valid-describe-callback
+
+jest.mock('country-flag-emoji-polyfill', () => ({
+  polyfillCountryFlagEmojis: jest.fn(),
+}));
+
 describe('App', () =>
   it('Should display toolbar with header', () => {
     render(<App />);
