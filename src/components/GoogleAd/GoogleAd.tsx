b584414b99bb815481689784eb6eb790e9e37a95
Muthu Pandian <hellomuthu23@gmail.com>
2025-06-14 22:07:42 +0100

    fix: fix ad spacing

--- src/components/GoogleAd/GoogleAd.tsx
+++ src/components/GoogleAd/GoogleAd.tsx
@@ -16,7 +16,7 @@ export const GoogleAd = () => {
     <>
       <ins
         className='adsbygoogle google_ad_responsive'
-        style={{ display: 'block' }}
+        style={{ display: 'block', margin: '30px' }}
         data-ad-client='ca-pub-3152802418728088'
         data-ad-slot='2256716101'
       ></ins>
